c7e9625a4ec3963d0eec39440058c0e852342e07
Shaunak Kishore <kshaunak@gmail.com>
2014-11-22 12:45:22 -0500

    Get room generation working

--- scripts/genWorld.py
+++ scripts/genWorld.py
@@ -1,3 +1,4 @@
+import math
 import random
 import struct
 import sys
@@ -10,6 +11,9 @@ class Tileset(object):
 
   def blocked(self, tile):
     return tile == self.default_tile
+
+  def get_free_tile(self):
+    return random.randint(0, self.num_tiles - 2)
 
 
 class Map(object):
@@ -45,6 +49,32 @@ class Map(object):
           file.write(struct.pack('B', self.tiles[w][h]))
 
 
+class Room(object):
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+
+  def distance(self, room):
+    x_distance = max(
+      (room.x - self.x - self.width),
+      (self.x - room.x - room.width), 0)
+    y_distance = max(
+      (room.y - self.y - self.height),
+      (self.y - room.y - room.height), 0)
+    return math.sqrt(x_distance**2 + y_distance**2)
+
+  def place(self, map, rooms, tolerance):
+    self.x = random.randint(0, map.width - self.width)
+    self.y = random.randint(0, map.height - self.height)
+    for room in rooms:
+      if self.distance(room) <= tolerance:
+        return False
+    for w in xrange(self.width):
+      for h in xrange(self.height):
+        map.tiles[self.x + w][self.y + h] = map.tileset.get_free_tile()
+    return True
+
+
 def generate_random_map(width, height, tileset):
   map = Map(width, height, tileset)
   for w in xrange(width):
@@ -53,8 +83,58 @@ def generate_random_map(width, height, tileset):
   return map
 
 
+def sample(distribution):
+  total = sum(distribution.itervalues())
+  target = random.random()*total
+  for (value, relative_probability) in distribution.iteritems():
+    target -= relative_probability
+    if target < 0:
+      return value
+  print 'WARNING: sample failed to find value!'
+  return distribution.keys()[0]
+
+
+def generate_mostly_linear_tree(n, bias=2):
+  # Returns a list of edges [(0, 1), (1, 2), ...] that make up the tree.
+  result = []
+  depths = {0: 0}
+  while len(result) < n:
+    child = len(result) + 1
+    distribution = \
+        dict((node, bias**depth) for (node, depth) in depths.iteritems())
+    parent = sample(distribution)
+    result.append((parent, child))
+    depths[child] = depths[parent] + 1
+  return result
+
+
+def generate_rooms_map(width, height, tileset):
+  map = Map(width, height, tileset)
+  (min_size, max_size) = (2, 4)
+  tries = width*height/(min_size**2)
+
+  rooms = []
+  while tries > 0:
+    width = random.randint(min_size, max_size)
+    height = random.randint(min_size, max_size)
+    room = Room(width, height)
+    if room.place(map, rooms, tolerance=min_size):
+      rooms.append(room)
+    else:
+      tries -= 1
+
+  room_graph = []
+  for room in rooms:
+    room_graph.append([])
+    for other in rooms:
+      room_graph[-1].append(room.distance(other))
+  print room_graph
+
+  return map
+
+
 if __name__ == '__main__':
   random.seed()
-  map = generate_random_map(16, 16, Tileset())
+  map = generate_rooms_map(32, 32, Tileset())
   print map
-  map.print_to_file('world.dat')
+  #map.print_to_file('world.dat')
