c9ebb9302a6481d3e8796c8b385cde45f89032c2
Shaunak Kishore <kshaunak@gmail.com>
2014-12-06 16:40:46 -0500

    Delete unused functions in BattleExecutor

--- BattleExecutor.cpp
+++ BattleExecutor.cpp
@@ -15,98 +15,6 @@ namespace skishore {
 namespace battle {
 
 namespace {
-
-// Takes an integer and a divisor and does division and rounding.
-inline int divround(int a, int b) {
-    return (a + (a > 0 ? b/2 : -b/2))/b;
-}
-
-// Takes a position and returns the square on the perimeter of the given room
-// that is closest to that position.
-Point ClosestPerimeterSquare(const Point& position, const TileMap::Room& room) {
-  Point result = kGridTicks*room.position;
-  if (position.x > result.x + kGridTicks*room.size.x/2) {
-    result.x += kGridTicks*room.size.x;
-  }
-  if (position.y > result.y + kGridTicks*room.size.y/2) {
-    result.y += kGridTicks*room.size.y;
-  }
-  if (abs(result.x - position.x) > abs(result.y - position.y)) {
-    result.x = position.x;
-  } else {
-    result.y = position.y;
-  }
-  result.x = divround(result.x, kGridTicks);
-  result.y = divround(result.y, kGridTicks);
-  Point other_corner = room.position + room.size - Point(1, 1);
-  result.x = min(max(room.position.x, result.x), other_corner.x);
-  result.y = min(max(room.position.y, result.y), other_corner.y);
-  return result;
-}
-
-// Advance distance steps along the room's perimeter in the ccw direction.
-Point AdvanceAlongPerimeter(
-    const Point& square, const TileMap::Room& room, int distance) {
-  if (room.size.x <= 1 || room.size.y <= 1) {
-    return square;
-  }
-  Point result = square;
-  while (distance > 0) {
-    if (result.x == room.position.x) {
-      int new_y = max(result.y - distance, room.position.y);
-      distance -= result.y - new_y;
-      result.y = new_y;
-    } else if (result.x == room.position.x + room.size.x - 1) {
-      int new_y = min(result.y + distance, room.position.y + room.size.y - 1);
-      distance -= new_y - result.y;
-      result.y = new_y;
-    }
-    if (result.y == room.position.y) {
-      int new_x = min(result.x + distance, room.position.x + room.size.x - 1);
-      distance -= new_x - result.x;
-      result.x = new_x;
-    } else if (result.y == room.position.y + room.size.y - 1) {
-      int new_x = max(result.x - distance, room.position.x);
-      distance -= result.x - new_x;
-      result.x = new_x;
-    }
-  }
-  return result;
-}
-
-void ComputePlaces(const TileMap::Room& room, const vector<Sprite*>& sprites,
-                   vector<Point>* places) {
-  ASSERT(sprites.size() > 0, "Got an empty sprites list!");
-  const int n = sprites.size();
-  places->resize(n);
-  vector<Point> options(n - 1);
-
-  Point half_square(kGridTicks/2, kGridTicks/2);
-  (*places)[0] = Point(room.position.x + room.size.x - 1,
-                       room.position.y + room.size.y/2);
-
-  for (int i = 0; i < n - 1; i++) {
-    const int offset = (i == 0 || i == n - 2 ? 3 : 1);
-    options[i] = Point(room.position.x + offset, room.position.y + 2*i);
-  }
-
-  vector<vector<Cost>> distances(n - 1, vector<Cost>(n - 1));
-  for (int i = 0; i < n - 1; i++) {
-    Point position = sprites[i + 1]->GetPosition() + half_square;
-    for (int j = 0; j < n - 1; j++) {
-      double distance = (position - kGridTicks*options[j]).length();
-      distances[i][j] = (int)distance;
-    }
-  }
-  Hungarian hungarian(n - 1, distances);
-  Hungarian::Status status = hungarian.Solve();
-  ASSERT(status == Hungarian::OK, "Hungarian exited with status: " << status);
-
-  for (int i = 0; i < n - 1; i++) {
-    (*places)[i + 1] = options[hungarian.GetXMatch(i)];
-  }
-}
-
 namespace script {
 
 class ParallelScript : public BattleScript {
@@ -163,6 +71,39 @@ class WaitForStateTransitionScript : public BattleScript {
 
 inline BattleScript* Run(Sprite* sprite, SpriteState* state) {
   return new script::WaitForStateTransitionScript(sprite, state);
+}
+
+void ComputePlaces(const TileMap::Room& room, const vector<Sprite*>& sprites,
+                   vector<Point>* places) {
+  ASSERT(sprites.size() > 0, "Got an empty sprites list!");
+  const int n = sprites.size();
+  places->resize(n);
+  vector<Point> options(n - 1);
+
+  Point half_square(kGridTicks/2, kGridTicks/2);
+  (*places)[0] = Point(room.position.x + room.size.x - 1,
+                       room.position.y + room.size.y/2);
+
+  for (int i = 0; i < n - 1; i++) {
+    const int offset = (i == 0 || i == n - 2 ? 3 : 1);
+    options[i] = Point(room.position.x + offset, room.position.y + 2*i);
+  }
+
+  vector<vector<Cost>> distances(n - 1, vector<Cost>(n - 1));
+  for (int i = 0; i < n - 1; i++) {
+    Point position = sprites[i + 1]->GetPosition() + half_square;
+    for (int j = 0; j < n - 1; j++) {
+      double distance = (position - kGridTicks*options[j]).length();
+      distances[i][j] = (int)distance;
+    }
+  }
+  Hungarian hungarian(n - 1, distances);
+  Hungarian::Status status = hungarian.Solve();
+  ASSERT(status == Hungarian::OK, "Hungarian exited with status: " << status);
+
+  for (int i = 0; i < n - 1; i++) {
+    (*places)[i + 1] = options[hungarian.GetXMatch(i)];
+  }
 }
 
 }  // namespace
