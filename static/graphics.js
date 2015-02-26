f3bd78d265759dd74ac619b017e91f1d5a0bb1f8
Shaunak Kishore <kshaunak@gmail.com>
2015-02-26 00:41:37 -0500

    Update graphic.js to produce first view

--- static/graphics.js
+++ static/graphics.js
@@ -1,135 +1,72 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>pixi.js example 2 loading a sprite sheet</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            background-color: #000000;
-        }
-    </style>
-    <script src="../../bin/pixi.dev.js"></script>
-</head>
-<body>
-    <script>
-    //PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+"use strict";
 
-    // create an array of assets to load, in the form of json files generated from TexturePacker
-    var assetsToLoader = ["tileset.json"];
+function BabelGraphics() {
+  // Core graphics constants.
+  this.tile_textures = [];
+  this.sprite_textures = [];
+  // These should be read from the JSON files instead of hardcoded.
+  this.num_tiles = 6;
+  this.num_sprites = 3;
+  this.radius = 9;
+  this.size = 2*radius + 1;
+  this.square = 16;
 
-    // create a new loader
-    loader = new PIXI.AssetLoader(assetsToLoader);
+  // The actual tile and sprite PIXI.Sprite instances.
+  this.tiles = [];
+  this.sprites = [];
 
-    // use callback
-    loader.onComplete = onAssetsLoaded;
+  var assets_to_load = ['tileset.json', 'sprites.json'];
+  var .loader = new PIXI.AssetLoader(assets_to_load);
+  loader.onComplete = this.OnAssetsLoaded.bind(this);
+  loader.load();
 
-    // begin load
-    loader.load();
+  this.stage = new PIXI.Stage(0x00000000);
 
-    // holder to store aliens
-    var aliens = [];
-    var alienFrames = [];
-    var count = 0;
+  this.width = (size - 2)*square;
+  this.height = (size - 2)*square;
+  this.renderer = PIXI.autoDetectRenderer(width, height);
 
-    // create an new instance of a pixi stage
-    var stage = new PIXI.Stage(0xFFFFFF);
+  //this.renderer.view.style.position = "absolute"
+  //this.renderer.view.style.width = window.innerWidth + "px";
+  //this.renderer.view.style.height = window.innerHeight + "px";
+  //this.renderer.view.style.display = "block";
 
-    // create a renderer instance.
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    if (height > 1080) {
-      width = Math.floor(width/2);
-      height = Math.floor(height/2);
+  // Insert the new renderer at the top of the DOM.
+  document.body.prependChild(this.renderer.view);
+}
+
+BabelGraphics.prototype.OnAssetsLoaded = function() {
+  for (var i = 0; i < num_tiles; i++) {
+    tile_textures.push(PIXI.Texture.fromFrame("tile" + i + ".png"));
+  }
+  for (var i = 0; i < num_sprites; i++) {
+    sprite_textures.push(PIXI.Texture.fromFrame("sprite" + i + ".png");
+  }
+  for (var x = 0; x < size; x++) {
+    for (var y = 0; y < size; y++) {
+      var tile = new PIXI.Sprite(tile_textures[0]);
+      tile.visible = false;
+      this.tiles.push(tile);
+      this.stage.addChild(tile);
     }
-    var renderer = PIXI.autoDetectRenderer(width, height);
+  }
 
-    renderer.view.style.position = "absolute"
-    renderer.view.style.width = window.innerWidth + "px";
-    renderer.view.style.height = window.innerHeight + "px";
-    renderer.view.style.display = "block";
+  requestAnimationFrame(this.Animate.bind(this));
+}
 
-    // add the renderer view element to the DOM
-    document.body.appendChild(renderer.view);
-
-    // create an empty container
-    var alienContainer = new PIXI.DisplayObjectContainer();
-    alienContainer.position.x = width/2;
-    alienContainer.position.y = height/2;
-
-    stage.addChild(alienContainer);
-
-    var label = new PIXI.Text("Click to enable cacheAsBitmap", {fill:"black", stroke:"white", strokeThickness:5});
-    label.position.x = 40;
-    label.position.y = height - 40;
-    stage.addChild(label)
-
-    function onAssetsLoaded()
-    {
-        for (var i = 0; i < 6; i++) {
-          alienFrames.push(PIXI.Texture.fromFrame("tile" + i + ".png"));
-        }
-
-        // add a bunch of aliens with textures from image paths
-        for (var i = 0; i < 324; i++)
-        {
-            // Create a tile with a random texture.
-            var texture = alienFrames[i % alienFrames.length];
-            var alien = new PIXI.Sprite(texture);
-            //alien.tint = Math.random() * 0xFFFFFF;
-
-            /*
-             * fun fact for the day :)
-             * another way of doing the above would be
-             * var alien = PIXI.Sprite.fromFrame(frameName);
-             */
-            alien.position.x = (Math.random() - 0.5)*width;
-            alien.position.y = (Math.random() - 0.5)*height;
-            alien.anchor.x = 0.5;
-            alien.anchor.y = 0.5;
-            aliens.push(alien);
-            alienContainer.addChild(alien);
-        }
-
-        // start animating
-        requestAnimFrame(animate);
+BabelGraphics.prototype.Animate = function() {
+  var view = Module.BabelGetView(this.radius);
+  for (var x = 0; x < size; x++) {
+    for (var y = 0; y < size; y++) {
+      var tile = this.tiles[this.size*x + y];
+      var tile_view = view.tiles.get(x).get(y);
+      if (tile_view.graphic < 0) {
+        tile.visible = false;
+      } else {
+        tile.setTexture(this.tile_textures[tile_view.graphic]);
+      }
     }
-
-    stage.mousedown = stage.touchstart = function()
-    {
-        if (alienContainer.cacheAsBitmap) {
-            label.setText("Click to enable cacheAsBitmap");
-            alienContainer.cacheAsBitmap = false;
-        } else {
-            label.setText("Click to disable cacheAsBitmap");
-            alienContainer.cacheAsBitmap = true;
-        }
-    }
-
-    function animate() {
-        // let's rotate the aliens a little bit
-        for (var i = 0; i < aliens.length; i++)
-        {
-            var alien = aliens[i];
-            alien.rotation += 0.1;
-            if (Math.random() > 0.0) {
-              var index = Math.floor(alienFrames.length*Math.random());
-              alien.setTexture(alienFrames[index]);
-            }
-        }
-
-        count += 0.01;
-        alienContainer.scale.x = Math.sin(count);
-        alienContainer.scale.y = Math.sin(count);
-
-        alienContainer.rotation += 0.01;
-        
-        // render the stage
-        renderer.render(stage);
-
-        requestAnimFrame(animate);
-    }
-    </script>
-
-    </body>
-</html>
+  }
+  view.delete();
+  renderer.render(stage);
+}
